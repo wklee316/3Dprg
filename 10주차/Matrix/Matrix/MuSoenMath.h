bbc7dc0dc2d0531c863a05fba61724f594295bf9
idioteque <39259366+wklee316@users.noreply.github.com>
2022-11-04 04:01:16 +0900

    Matrix 자료구조 구현

---
+++ 10주차/Matrix/Matrix/MuSoenMath.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#define PI 3.141592						//원주율을 소수점 6자리까지 근사값으로 정의
+
+typedef struct mat3 {					//float 자료형을 가지는 2차원 행렬 구조체 선언
+	float mat[3][3];
+
+	mat3(float a[3][3]) {		//생성자에서 행렬의 value 초기화
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) this->mat[i][j] = a[i][j];
+		}
+	}
+
+
+	mat3 operator*(mat3& ref) {			//연산자 오버로딩으로 3x3 행렬끼리의 곱 구현
+		mat3 resultMat(new float[3][3]{ {1, 0, 0}, {0,1,0}, {0,0,1} });		//행렬 곱의 결과값을 저장하는 변수 선언
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				resultMat.mat[i][j] = 0;
+				for (int k = 0; k < 3; k++)
+					resultMat.mat[i][j] += this->mat[i][k] * ref.mat[k][j];
+			}
+		}
+
+		return resultMat;					//행렬 곱의 결과 값 반환
+	}
+
+};
+
+typedef struct vec3 {					//float 자료형을 가지는 2차원좌표 + 동차좌표 구조체 선언
+	float vec[3];
+
+	vec3(float a[3]) {					//생성자에서 Vector의 value 초기화
+		for (int i = 0; i < 3; i++) this->vec[i] = a[i];
+	}
+
+	vec3 operator*(mat3& ref) {			//연산자 오버로딩으로 3x1 Vector와 3x3 행렬곱 구현
+		vec3 resultVec(new float[3] { 0, 0, 0 });	//행렬 곱의 결과값을 저장하는 변수 선언
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				resultVec.vec[i] += this->vec[j] * ref.mat[j][i];
+			}
+		}
+
+		return resultVec;		//행렬 곱의 결과 값 반환
+	}
+
+};
---
+++ 10주차/Matrix/Matrix/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>				//기본 입출력 라이브러리
+#include <math.h>				//회전 행렬에서 사용할 삼각함수를 위한 라이브러리
+#include "MuSoenMath.h"			//자료구조를 구현한 헤더파일
+
+
+int main() {
+	//3,5 만큼 이동하는 이동행렬
+	mat3 Translate(new float[3][3]{ {1, 0, 0}, {0,1,0}, {3,5,1} });
+	//30도 만큼 회전하는 회전행렬, 30도는 Radian 값으로 원주율/6이기에 회전행렬에 해당하는 값을 넣음
+	mat3 Rotate(new float[3][3]{ {(float)cos(PI/6), -(float)sin(PI / 6), 0}, {(float)sin(PI / 6), (float)cos(PI / 6), 0}, {0, 0, 1}});
+	//2배 확대하는 스케일행렬
+	mat3 Scale(new float[3][3]{ {2, 0, 0}, {0, 2, 0}, {0, 0, 1} });
+
+	vec3 v(new float[3] {4, 2, 1});		//시작 좌표를 4,2로 선언
+	vec3 p(new float[3] {0, 0, 0});		//결과 좌표값 선언 및 초기화
+
+	p = v * Translate * Rotate * Scale;	//3,5 이동 | 30도 회전 | 2배 확대
+
+
+	for (int i = 0; i < 3; i++) {		//결과 좌표값 출력
+		std::cout << p.vec[i];
+		if (i < 2) std::cout << " ";
+	}
+}
